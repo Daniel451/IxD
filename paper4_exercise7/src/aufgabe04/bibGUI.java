d18c44f7ec00cb3f47f7a566d6787b99da06152c
fabianbehrendt <fabian.behrendt95@gmail.com>
2016-05-04 00:56:26 +0200

    zwei Popupfenster erstellt

--- paper4_exercise7/src/aufgabe04/bibGUI.java
+++ paper4_exercise7/src/aufgabe04/bibGUI.java
@@ -1,6 +1,8 @@
 package aufgabe04;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.event.*;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -119,9 +121,10 @@ public class bibGUI extends JFrame implements ActionListener {
 		ourLayout.setVgap(50);
 		
 		// TODO VerticalFlowLayot
-		JPanel allConentPane = new JPanel(new FlowLayout());
+		JPanel allContentPane = new JPanel(new FlowLayout());
 		JPanel contentPane = new JPanel(ourLayout);
-		allConentPane.add(contentPane);
+		allContentPane.add(contentPane);
+		allContentPane.setBorder(new EmptyBorder(20, 20, 20, 20));
 		
 		
 		contentPane.setPreferredSize(new Dimension(400, 200));
@@ -131,26 +134,154 @@ public class bibGUI extends JFrame implements ActionListener {
 		contentPane.add(returnBook);
 		contentPane.add(rentBook);
 		
-		content.add(allConentPane);
+		content.add(allContentPane);
 		
-		content.setSize(800, 600);
+		//content.setSize(800, 600);
+		content.pack();
+		content.setResizable(false);
 		content.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		content.setVisible(true);
 	}
 	
 	public void actionPerformed(ActionEvent e)
 	{
-		JDialog bla = new JDialog();
+		JDialog popup = new JDialog();
 				
 		switch(e.getActionCommand())
 		{
-			// TODO 4x kopieren
 			case "Benutzer hinzufügen":
-				bla = new JDialog(content, "Benutzer hinzufügen" ,true);
+				popup = addUserWindow();
 				System.out.println("Benutzer hinzufügen wurde gedrückt");
+				break;
+			case "Buch hinzufügen":
+				popup = addBookWindow();
+				System.out.println("Buch hinzufügen wurde gedrückt");
+				break;
+			case "Rückgabe":
+				popup = new JDialog(content, "Rückgabe" ,true);
+				System.out.println("Rückgabe wurde gedrückt");
+				break;
+			case "Verleih":
+				popup = new JDialog(content, "Verleih" ,true);
+				System.out.println("Verleih wurde gedrückt");
 				break;
 		}
 		
-		bla.setVisible(true);
+		popup.setVisible(true);
+	}
+	
+	public JDialog addUserWindow() {
+		JDialog popupAddUser = new JDialog(content, "Benutzer hinzufügen" ,true);
+		
+		JPanel contentAddUser = new JPanel(new GridLayout(7, 2, 10, 10));
+		contentAddUser.setBorder(new EmptyBorder(20, 20, 20, 20));
+		
+		JLabel name = new JLabel("Name des Benutzers:");
+		JTextField nameField = new JTextField();
+		
+		JLabel id = new JLabel("ID des Benutzers:");
+		JSpinner idSpinner = new JSpinner();
+		
+		JLabel address = new JLabel("Adresse des Benutzers:");
+		JTextField addressField = new JTextField();
+		
+		JLabel birthday = new JLabel("Geburtstag des Benutzers:");
+		JTextField birthdayField = new JTextField();
+		
+		JLabel registrationDate = new JLabel("Aufnahmedatum des Benutzers:");
+		JTextField registrationDateField = new JTextField();
+		
+		JLabel gender = new JLabel("Geschlecht des Benutzers:");
+		JRadioButton maleButton = new JRadioButton("männlich");
+		JRadioButton femaleButton = new JRadioButton("weiblich");
+		ButtonGroup genderGroup = new ButtonGroup();
+		genderGroup.add(maleButton);
+		genderGroup.add(femaleButton);
+		
+		JPanel genderPanel = new JPanel(new GridLayout(2, 0));
+		genderPanel.add(maleButton);
+		genderPanel.add(femaleButton);
+		
+		JButton add = new JButton("Aufnehmen");
+		JButton cancel = new JButton("Abbrechen");
+		
+		contentAddUser.add(name);
+		contentAddUser.add(nameField);
+		contentAddUser.add(id);
+		contentAddUser.add(idSpinner);
+		contentAddUser.add(address);
+		contentAddUser.add(addressField);
+		contentAddUser.add(birthday);
+		contentAddUser.add(birthdayField);
+		contentAddUser.add(registrationDate);
+		contentAddUser.add(registrationDateField);
+		contentAddUser.add(gender);
+		contentAddUser.add(genderPanel);
+		contentAddUser.add(add);
+		contentAddUser.add(cancel);
+		
+		popupAddUser.add(contentAddUser);
+		popupAddUser.pack();
+		popupAddUser.setResizable(false);
+		
+		return popupAddUser;
+	}
+	
+	public JDialog addBookWindow() {
+		JDialog popupAddBook = new JDialog(content, "Buch hinzufügen" ,true);
+		
+		JPanel contentAddBook = new JPanel(new GridLayout(6, 2, 10, 10));
+		contentAddBook.setBorder(new EmptyBorder(20, 20, 20, 20));
+		
+		JLabel title = new JLabel("Titel des Buches:");
+		JTextField titleField = new JTextField();
+		
+		JLabel isbn = new JLabel("ISBN des Buches:");
+		JTextField isbnField = new JTextField();
+		
+		JLabel rentalState = new JLabel("Verliehen?");
+		JCheckBox rentalStateCheckBox = new JCheckBox();
+		
+		JLabel borrower = new JLabel("ID des Entleihers:");
+		JSpinner borrowerSpinner = new JSpinner();
+		
+		JLabel keyWords = new JLabel("Schlagworte:");
+		String[] keyWordsStrings = { "Krimi", "Fantasy", "Sci-Fi", "Kurzgeschichte", "Horror", "Kinderbuch"};
+		JComboBox<String> keyWordsDropdown= new JComboBox<String>(keyWordsStrings);
+		keyWordsDropdown.setEnabled(true);
+		
+		JButton add = new JButton("Aufnehmen");
+		JButton cancel = new JButton("Abbrechen");
+		
+		contentAddBook.add(title);
+		contentAddBook.add(titleField);
+		contentAddBook.add(isbn);
+		contentAddBook.add(isbnField);
+		contentAddBook.add(rentalState);
+		contentAddBook.add(rentalStateCheckBox);
+		contentAddBook.add(borrower);
+		contentAddBook.add(borrowerSpinner);
+		contentAddBook.add(keyWords);
+		contentAddBook.add(keyWordsDropdown);
+		contentAddBook.add(add);
+		contentAddBook.add(cancel);
+		
+		popupAddBook.add(contentAddBook);
+		popupAddBook.pack();
+		popupAddBook.setResizable(false);
+		
+		return popupAddBook;
+	}
+	
+	public JDialog returnWindow() {
+		JDialog popupAddUser = new JDialog(content, "Benutzer hinzufügen" ,true);
+		
+		return popupAddUser;
+	}
+	
+	public JDialog rentalWindow() {
+		JDialog popupAddUser = new JDialog(content, "Benutzer hinzufügen" ,true);
+		
+		return popupAddUser;
 	}
 }
